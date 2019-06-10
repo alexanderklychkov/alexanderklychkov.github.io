bfb74540166e74510d88f7cf4370191e6e2e1127
alexanderklychkov <30840639+alexanderklychkov@users.noreply.github.com>
2019-06-10 10:28:26 +0300

    Add files via upload

---
+++ taskboard/precache-manifest.fdad941b0f45d15cd9900f3cadf54657.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f859d459df9cefac69c9156744e56322",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82fb77b1091f689cc1ff",
+    "url": "/static/css/main.c07105db.chunk.css"
+  },
+  {
+    "revision": "f92b4f139bc402851296",
+    "url": "/static/js/2.aa0487db.chunk.js"
+  },
+  {
+    "revision": "82fb77b1091f689cc1ff",
+    "url": "/static/js/main.2d3f1e4f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "07d2ba497042d1c50cda3a9d8f2b999f",
+    "url": "/static/media/bg.07d2ba49.jpg"
+  }
+]);
