167ddd7bf1d8348f7d967f5c39b895f8ce516a9c
Alexander Klychkov <30840639+alexanderklychkov@users.noreply.github.com>
2023-08-30 10:44:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 try {
   const result = await import('https://cdn.jsdelivr.net/npm/saby-customizer@0.0.0/lib/saby-lib/edo.js');
   console.log(result);
-} catch(e) => console.error(e)
+} catch(e) {
+  console.error(e);
+}
