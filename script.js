cdef0dd419542dc8b53102d28539d82adf3de6b3
Alexander Klychkov <30840639+alexanderklychkov@users.noreply.github.com>
2023-08-30 10:58:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 (async () => {
   try {
-    const result = await import('https://cdn.jsdelivr.net/npm/saby-customizer@0.0.0/lib/saby-lib/edo.js');
-    console.log(result);
+    await import('https://cdn.jsdelivr.net/npm/saby-customizer@0.0.0/lib/saby-lib/edo.js');
+    const popupContainer = document.getElementById('popup');
+    let observer;
+
+    if (!observer) {
+      observer = new MutationObserver(() => {
+        if (!timeout) {
+          timeout = setTimeout(() => {
+            console.log('test');
+            timeout = null
+          }, 1000)
+        }
+      });
+    }
+
+    if (popupContainer) {
+      this.observer.observe(popupContainer, { childList: true, subtree: true })
+    }
+    
   } catch(e) {
     console.error(e);
   }
